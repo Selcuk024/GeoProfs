0ab9bfef208e7b2ff2a75a7ec9b0140843a75326
SergioGomezAlvarez <sgmichielr@gmail.com>
2024-11-28 16:12:20 +0100

    Added personal data UI to profile page

--- GeoProfs/src/components/ProfilePage.vue
+++ GeoProfs/src/components/ProfilePage.vue
@@ -1,53 +1,80 @@
 <template>
-      <div class="container">
+  <div class="container">
     <h1 class="username">Welkom Marco</h1>
-    <div class="class">
-        <div class="verlof-item" v-for="(item, index) in verlofList" :key="index">
-      <div class="verlof-content">
-        <p class="title">{{ item.title }}</p>
-        <p class="date">Data: {{ item.date }}</p>
-        <p class="status">
-          Status: <span :class="item.status">{{ item.status }}</span>
-        </p>
-      </div>
+  </div>
+  <div class="personal-data-container">
+    <div class="personal-data-col">
+      <div class="personal-data"></div>
+      <div class="personal-data"></div>
     </div>
+    <div class="personal-data-col">
+      <div class="personal-data"></div>
+      <div class="personal-data"></div>
+    </div>
+    <div class="personal-data-col">
+      <div class="personal-data"></div>
+      <div class="personal-data"></div>
     </div>
   </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'AfwezigPage'
-  }
-  </script>
-  
-  <style scoped>
-*{
+</template>
+
+<script>
+export default {
+  name: 'ProfilePage'
+}
+</script>
+
+<style scoped>
+* {
   margin: 0;
   padding: 0;
+}
+.container {
+  width: 100%;
+  background-color: #F1F1F1;
+  height: 20%;
+  display: flex;
+  flex-direction: column;
+}
 
-}
-.container{
-  width: 100%;
-  background-color: red;
-  height: 100%;  
-  display: flex;
-  flex-direction: column;
-}
-.class{
-  background-color: #4a4a4a;
-  width: 100%;
-  height: 70%;
-  align-self: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: auto;
-}
-.username{
+.username {
   font-size: 48px;
   margin-left: 24px;
   margin-top: 24px;
+}
+
+.personal-data-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 80%;
+  gap: 50px;
+  background-color: #F1F1F1;
+}
+
+.personal-data-col {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 25%;
+  height: 100%;
+  gap: 50px;
+  background-color: #F1F1F1;
+}
+
+.personal-data {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 40%;
+  background-color: #FFFFFF;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); /* Schaduw toevoegen */
+  border-radius: 8px; /* Optioneel: afgeronde hoeken */
 }
 
 </style>
