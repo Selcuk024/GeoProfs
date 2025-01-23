fb2ebb93f2bc48905aa0eefca3eefc6049ca0ae7
Selcuk <1202320@student.roc-nijmegen.nl>
2025-01-23 13:47:38 +0100

    changed jest config file

--- GeoProfs/tests/unit/jest.config.js
+++ GeoProfs/tests/unit/jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  transform: {
-    '^.+\\.vue$': '@vue/vue3-jest',
-    '^.+\\.js$': 'babel-jest',
-  },
+  testEnvironment: 'jsdom',
   moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.js$': 'babel-jest'
+  }
 };
