c3646bf5d773601768ece49ee46ca2a2fb37f989
Dylan Backus <dylanbackus024@gmail.com>
2025-01-28 12:38:43 +0100

    headercomponent responsive

--- GeoProfs/src/components/HeaderComponent.vue
+++ GeoProfs/src/components/HeaderComponent.vue
@@ -33,73 +33,112 @@ export default {
 }
 </script>
 
-<style scoped>
-.header {
-  width: 90%;
-  display: flex;
-  align-items: center;
-  justify-content: end;
-  padding: 16px;
-  background-color: #001f6b;
-  color: white;
-}
-
-.aroundLogo {
-  height: 15vh;
-  width: 10%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.logo {
-  width: 55%;
-  border-radius: 50%;
-  margin-top: 12px;
-  margin-bottom: 12px;
-}
-
-.profile-icon {
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  margin-left: auto;
-  cursor: pointer;
-}
-
+<style scoped lang="scss">
 .headerContainer {
   display: flex;
   flex-direction: row;
+
+  .aroundLogo {
+    height: 15vh;
+    width: 10%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .logo {
+      width: 55%;
+      border-radius: 50%;
+      margin-top: 12px;
+      margin-bottom: 12px;
+    }
+  }
+
+  .header {
+    width: 90%;
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    padding: 16px;
+    background-color: #001f6b;
+    color: white;
+
+    .profile-icon {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      margin-left: auto;
+      cursor: pointer;
+    }
+
+    .dropdown {
+      height: 50px;
+      width: 50px;
+      position: relative;
+      display: flex;
+      background-color: transparent;
+
+      &:hover .dropdown-content {
+        display: block;
+      }
+
+      .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #f9f9f9;
+        width: 6vw;
+        height: 5vh;
+        border: none;
+        z-index: 1;
+        top: 100%;
+        right: 0;
+        cursor: pointer;
+
+        .logout-text {
+          font-weight: 400;
+          color: #000;
+          cursor: pointer;
+        }
+      }
+    }
+  }
 }
 
-.dropdown {
-  height: 50px;
-  width: 50px;
-  position: relative;
-  display: flex;
-  background-color: transparent;
-}
+@media (max-width: 768px) {
+  .headerContainer {
+    flex-direction: row;
 
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  width: 6vw;
-  height: 5vh;
-  border: none;
-  z-index: 1;
-  top: 100%;
-  right: 0;
-  cursor: pointer;
-}
+    .aroundLogo {
+      height: auto;
+      width: 20%;
 
-.logout-text {
-  font-weight: 400;
-  color: #000;
-  cursor: pointer;
-}
+      .logo {
+        width: 70%;
+      }
+    }
 
-.dropdown:hover .dropdown-content {
-  display: block;
+    .header {
+      width: 100%;
+      padding: 8px;
+
+      .profile-icon {
+        width: 40px;
+        height: 40px;
+      }
+
+      .dropdown {
+        width: auto;
+        height: auto;
+
+        .dropdown-content {
+          width: 80%;
+          height: auto;
+        }
+
+        .logout-text {
+          text-align: center;
+        }
+      }
+    }
+  }
 }
 </style>
